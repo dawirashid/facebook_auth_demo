ad9f5f03716bdc7e3ea91ed44a054f6484011d43
dawirashid <77828860+dawirashid@users.noreply.github.com>
2022-01-13 17:56:59 +0800

    add facebook auth and done

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_facebook_auth/flutter_facebook_auth.dart';
+//the link https://pub.dev/packages/flutter_facebook_auth/install
 
 void main() {
   runApp(const MyApp());
@@ -24,8 +26,47 @@ class FacebookSignIn extends StatefulWidget {
 }
 
 class _FacebookSignInState extends State<FacebookSignIn> {
+  // 1.set up data var
+  Map? _userData;
+
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text(
+              'Facebook (Logged ' + (_userData == null ? 'Out' : 'In') + ')'),
+        ),
+        body: Center(
+          child: Column(
+            children: [
+              ElevatedButton(
+                onPressed: () async {
+                  final result = await FacebookAuth.i
+                      .login(permissions: ['email', 'public_profile']);
+                  if (result.status == LoginStatus.success) {
+                    final requestData =
+                        await FacebookAuth.i.getUserData(fields: "email,name");
+                    setState(() {
+                      _userData = requestData;
+                    });
+                  }
+                },
+                child: const Text('Log In'),
+              ),
+              ElevatedButton(
+                onPressed: () async {
+                  await FacebookAuth.i.logOut();
+                  setState(() {
+                    _userData = null;
+                  });
+                },
+                child: const Text('Log Out'),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
   }
 }
